e685c887aa1daf63306a4559f9930c1109c5db6f
MelanyVinueza <mavinueza10@espe.edu.ec>
2026-01-18 16:15:46 -0500

    fix: restore missing package and imports in SwitchRefundRequest.java

--- ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/dto/SwitchRefundRequest.java
+++ ms-transaccion/src/main/java/com/arcbank/cbs/transaccion/dto/SwitchRefundRequest.java
@@ -1,3 +1,10 @@
+package com.arcbank.cbs.transaccion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Data
